8e1bb4f51a34e2b24268ad2dfee21aafeabf5a72
Jeremiah Heller <jeremiah@inertialbit.net>
2010-10-19 17:20:51 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.1'
+
+gem 'sqlite3-ruby', :require => 'sqlite3'
+
+gem "formtastic"
+
+group :development, :test do
+  gem "rspec-rails"
+  gem "acts_as_fu"
+  gem "cucumber-rails"
+  gem "capybara"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FileShare::Application.config.secret_token = 'bbce55f6968bd726783655882cc294e651853a2deb82d5853867023359e3fca0c01486b0fe9158af6d29fde3dc7347b6029917742f64a66fc2d58712d99565cb'
